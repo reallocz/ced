0eff12245f2a519a390bde82199baeed29bb600d
realloc <realloc@protonmail.com>
2018-03-08 14:48:48 +0530

    buffer.h api change and major refactoring
    - Seperate cursor and gap logic (see _sync_gap())
    - Make function names more consistent
    - Add _sync_gap for efficiency

--- src/buffer.c
+++ src/buffer.c
@@ -17,29 +17,30 @@
 #define SB(alias, hBuffer)\
     struct buffer* alias = _get_buffer(hBuffer)
 
-/** Evals to 1 if pos is inside buffer gap */
-#define INGAP(buffer, pos)\
-    ((pos >= buffer->cur && pos < buffer->cur + buffer->gap))
+/** Evals to 1 if i is inside buffer gap */
+#define INGAP(buffer, i)\
+    ((i >= buffer->gap.pos && i < buffer->gap.pos + buffer->gap.len))
 
-/** Store expensive to calculate stuff like linecount */
-struct buf_cache {
-    unsigned int linecount;
-};
 
 struct buffer {
     /*unique identifier of each buffer for debugging*/
     unsigned int id;
     /* 1 if buffer is in use (not destroyed)*/
     int in_use;
-
     enum buffer_type type;
     const char* name;		/* Name of buffer(file) */
     char* data;			/* File in memory */
     unsigned int size;		/* Size of buffer */
     unsigned int cur;		/* Cursor position */
-    unsigned int gap;		/* Size of the gap */
 
-    struct buf_cache cache;
+    struct {
+        unsigned int pos;       /* Beginning of gap */
+        unsigned int len;       /* Length of gap */
+    } gap;
+
+    struct {
+        unsigned int linecount;
+    } cache;
 };
 
 
@@ -69,63 +70,13 @@ int buf_exit()
     return 1;
 }
 
-
-/** return a new id for a buffer */
-    static
-unsigned int _generate_id()
-{
-    static unsigned int ids = 0;
-    return ids++;
-}
-
-/** return a pointer to the buffer in use of the handle */
-static
-struct buffer* _get_buffer(hBuffer buf)
-{
-    if(buf < 0 || buf >= G.bufcap) {
-        log_fatal(G.tag, "Invalid handle: %d", buf);
-    }
-    struct buffer* b = &G.buffers[buf];
-    if(! b->in_use) {
-        log_fatal(G.tag, "Bad handle: %d", buf);
-    }
-    return b;
-}
-
-
-/** return a handle to a free buffer (in_use == 0) */
-static
-hBuffer _get_free_handle()
-{
-    for(unsigned int i = 0; i < G.bufcap; ++i) {
-        if(! G.buffers[i].in_use) {
-            return i;
-        }
-    }
-    log_fatal(G.tag, "%s: OUT OF BUFFERS!", __func__);
-}
-
-
-/** Calculate number of char 'c' in the buffer */
-static
-unsigned int _calc_charcount(hBuffer buf, char ch)
-{
-    SB(b, buf);
-    assert(b);
-    unsigned int count = 0;
-    for(unsigned int i = 0; i < b->size; ++i) {
-        if((!INGAP(b, i)) && b->data[i] == ch)
-            count++;
-    }
-    return count;
-}
-
 void buf_destroy(hBuffer buf)
 {
     SB(b, buf);
     unsigned int id = b->id;
     b->in_use = 0;
     free(b->data);
+    log_l(G.tag, "Buffer destroyed (id=%d)", id);
 }
 
 
@@ -138,37 +89,17 @@ hBuffer buf_create(enum buffer_type type)
     b->in_use = 1;
     b->type = type; // TODO type specific setup
     b->name = "[NEWBUF]";
+
     b->size = BUFFER_GAPSIZE;
-    b->gap = BUFFER_GAPSIZE;
-    b->data = calloc(b->size, sizeof(char));
     b->cur = 0;
+    b->gap.pos = 0;
+    b->gap.len = BUFFER_GAPSIZE;
+
+    b->data = calloc(b->size, sizeof(char));
+    assert(b->data);
 
     b->cache.linecount = 0;
     return buf;
-}
-
-
-void buf_add_gap(hBuffer buf)
-{
-    SB(b, buf);
-    unsigned int newsize = b->size + BUFFER_GAPSIZE;
-    unsigned int newgap = b->gap + BUFFER_GAPSIZE;
-    char* newbuf = calloc(newsize, sizeof(b->data[0]));
-    assert(newbuf);
-    log_l(G.tag, "Gap added: %d -> %d", b->size, newsize);
-
-    /* Copy memory to new buffer */
-    // from start to cursor
-    memcpy(newbuf, b->data, b->cur * sizeof(char));
-    // from end of gap to end of buffer
-    memcpy(&newbuf[b->cur + newgap],
-           &b->data[b->cur + b->gap],
-           b->size - (b->cur + b->gap));
-    // free old buffer
-    free(b->data);
-    b->data = newbuf;
-    b->size = newsize;
-    b->gap = newgap;
 }
 
 
@@ -176,19 +107,19 @@ void buf_addch(hBuffer buf, char ch)
 {
     SB(b, buf);
     assert(b->data);
-    if(b->gap < 1) {
-        buf_add_gap(buf);
+    _sync_gap(b);
+    if(b->gap.len < 1) {
+        _gap_add(b);
     }
+
+    b->data[b->gap.pos] = ch;
+    b->gap.len--;
+    b->gap.pos++;
+    b->cur = b->gap.pos;
 
     if(ch == '\n') {
-        // Update cache and linecount
-        buf_update(buf);
+        _update_cache(b);
     }
-
-    b->data[b->cur] = ch;
-    b->cur++;
-    b->gap--;
-
     return;
 }
 
@@ -196,54 +127,53 @@ void buf_addch(hBuffer buf, char ch)
 void buf_delch(hBuffer buf)
 {
     SB(b, buf);
-    assert(b && b->data);
-    if(b->cur < 1) {
+    assert(b->data);
+    _sync_gap(b);
+    if(b->gap.pos < 1) {
         return;
     }
 
-    b->cur--;
-    b->gap++;
-    char delchar = b->data[b->cur];
+    b->gap.pos--;
+    b->gap.len++;
+    b->cur = b->gap.pos;
+
+    char delchar = b->data[b->gap.pos];
 
     if(delchar == '\n') {
-        // Update cache
-        buf_update(buf);
+        _update_cache(b);
     }
 
-    if(b->cur + b->gap > b->size) {
+    if(b->gap.pos + b->gap.len > b->size) {
         log_fatal(G.tag, "ERROR: FIX THIS!");
     }
 }
 
 
-void buf_move_cur_forward(hBuffer buf, unsigned int n)
+void buf_cur_mvf(hBuffer buf, unsigned int n)
 {
     SB(b, buf);
-    assert(b);
-    if(b->cur + b->gap == b->size) {
-        log_l(G.tag, "EOB!");
-        return;
-    }
-    b->data[b->cur] = b->data[b->cur + b->gap];
     b->cur++;
-}
-
-void buf_move_cur_back(hBuffer buf, unsigned int n)
-{
-    SB(b, buf);
-    assert(b);
-    if(b->cur == 0) {
-        return;
+    if(b->cur >= b->size) {
+        b->cur = b->size - 1;
     }
-    b->data[(b->cur + b->gap) - 1] = b->data[b->cur - 1];
-    b->cur--;
+    return;
 }
 
 
-unsigned int buf_get_cursor(hBuffer buf)
+void buf_cur_mvb(hBuffer buf, unsigned int n)
 {
     SB(b, buf);
-    assert(b);
+    b->cur--;
+    if(b->cur <= 0) {
+        b->cur = 0;
+    }
+    return;
+}
+
+
+unsigned int buf_cur_get(hBuffer buf)
+{
+    SB(b, buf);
     return b->cur;
 }
 
@@ -251,7 +181,6 @@ unsigned int buf_get_cursor(hBuffer buf)
 unsigned int buf_get_linecount(hBuffer buf)
 {
     SB(b, buf);
-    assert(b);
     return b->cache.linecount;
 }
 
@@ -276,9 +205,10 @@ struct buf_props buf_get_props(hBuffer buf)
     SB(b, buf);
     struct buf_props props = {
         .size = b->size,
-        .gap = b->gap,
         .cur = b->cur,
         .linecount = b->cache.linecount,
+        .gap.pos = b->gap.pos,
+        .gap.len = b->gap.len
     };
     return props;
 }
@@ -302,20 +232,11 @@ int buf_save_to_disk(hBuffer buf, const char* path)
 }
 
 
-int buf_update(hBuffer buf)
-{
-    SB(b, buf);
-    // Update cache
-    b->cache.linecount = _calc_charcount(buf, '\n') + 1;
-    return 0;
-}
-
-
 void buf_pprint(hBuffer buf)
 {
     SB(b, buf);
-    log_l(G.tag, "Buffer{id=%d, cur=%d, gap=%d, size=%d}",
-            b->id, b->cur, b->gap, b->size);
+    log_l(G.tag, "Buffer{id=%d, cur=%d, gap.pos=%d, gap.len = %d, size=%d}",
+            b->id, b->cur, b->gap.pos, b->gap.len, b->size);
 }
 
 
@@ -334,4 +255,116 @@ void buf_printbuf(const hBuffer buf)
         }
     }
     log_lc("\n");
+}
+
+
+/** buffer_internal.h **/
+
+static
+unsigned int _generate_id()
+{
+    static unsigned int ids = 0;
+    return ids++;
+}
+
+static
+struct buffer* _get_buffer(hBuffer buf)
+{
+    if(buf < 0 || buf >= G.bufcap) {
+        log_fatal(G.tag, "Invalid handle: %d", buf);
+    }
+    struct buffer* b = &G.buffers[buf];
+    if(! b->in_use) {
+        log_fatal(G.tag, "Bad handle: %d", buf);
+    }
+    return b;
+}
+
+
+static
+hBuffer _get_free_handle()
+{
+    for(unsigned int i = 0; i < G.bufcap; ++i) {
+        if(! G.buffers[i].in_use) {
+            return i;
+        }
+    }
+    log_fatal(G.tag, "%s: OUT OF BUFFERS!", __func__);
+}
+
+
+static
+unsigned int _calc_charcount(struct buffer* b, char ch)
+{
+    unsigned int count = 0;
+    for(unsigned int i = 0; i < b->size; ++i) {
+        if((!INGAP(b, i)) && b->data[i] == ch)
+            count++;
+    }
+    return count;
+}
+
+
+static
+int _update_cache(struct buffer* b)
+{
+    b->cache.linecount = _calc_charcount(b, '\n') + 1;
+    return 0;
+}
+
+
+static
+void _gap_add(struct buffer* b)
+{
+    unsigned int newsize = b->size + BUFFER_GAPSIZE;
+    unsigned int newgap = b->gap.len + BUFFER_GAPSIZE;
+
+    char* newbuf = calloc(newsize, sizeof(b->data[0]));
+    assert(newbuf);
+    log_l(G.tag, "Gap added: %d -> %d", b->size, newsize);
+
+    /* Copy memory to new buffer */
+    // from start to cursor
+    memcpy(newbuf, b->data, b->gap.pos * sizeof(char));
+    // from end of gap to end of buffer
+    memcpy(&newbuf[b->gap.pos + newgap],
+           &b->data[b->gap.pos + b->gap.len],
+           b->size - (b->gap.pos + b->gap.len));
+    // free old buffer
+    free(b->data);
+    b->data = newbuf;
+    b->size = newsize;
+    b->gap.len = newgap;
+}
+
+static
+int _sync_gap(struct buffer* b)
+{
+    int diff = b->cur - b->gap.pos;
+    unsigned int offset = 0;
+
+    // Cursor is ahead of the gap
+    if(diff > 0) {
+        offset = diff;
+        for(unsigned int i = 0; i < offset; ++i) {
+            b->data[b->gap.pos] = b->data[b->gap.pos + b->gap.len];
+            b->gap.pos++;
+        }
+        log_l(G.tag, "%s: diff=%d, offset=%d", __func__, diff, offset);
+    }
+
+    // Cursor is behind the gap
+    else if(diff < 0) {
+        offset = -1 * diff;
+        for(unsigned int i = 0; i < offset; ++i) {
+            b->data[b->gap.pos + b->gap.len - 1]
+                = b->data[b->gap.pos - 1];
+            b->gap.pos--;
+        }
+        log_l(G.tag, "%s: diff=%d offset=%d", __func__, diff, offset);
+    }
+    else {
+        // Cursor already in sync
+    }
+    return offset;
 }
--- src/buffer.h
+++ src/buffer.h
@@ -23,8 +23,11 @@ enum buffer_type {
 struct buf_props {
     unsigned int size;
     unsigned int cur;
-    unsigned int gap;
     unsigned int linecount;
+    struct {
+        unsigned int pos;
+        unsigned int len;
+    } gap;
 };
 
 /** Initialize buffer submodule */
@@ -33,33 +36,36 @@ int buf_exit();
 
 /** Create a new empty buffer of the specified type */
 hBuffer buf_create(enum buffer_type type);
-
 /** buf_destroy destorys the buffer and free's the data*/
 void buf_destory(hBuffer buf);
 
-void buf_add_gap(hBuffer buf);
+/** Adding and deleting characters **/
 /** Add a char at the current cursor position */
 void buf_addch(hBuffer buf, char ch);
 /** Delete a character at current cursor position */
 void buf_delch(hBuffer buf);
-/** Move cursor towards the beginning of buffer by 'n' chars */
-void buf_move_cur_forward(hBuffer buf, unsigned int n);
-/** Move cursor towards the end of buffer by 'n' chars */
-void buf_move_cur_back(hBuffer buf, unsigned int n);
-/** Returns the cursor position from the start of buffer (0-indexed) */
-unsigned int buf_get_cursor(hBuffer buf);
+
+/** Cursor **/
+/** Returns the cursor position from the start of buffer (0) */
+unsigned int buf_cur_get(hBuffer buf);
+/** Move cursor forward (towards EOB) by 'n' chars */
+void buf_cur_mvf(hBuffer buf, unsigned int n);
+/** Move cursor backward (beginning of buffer) by 'n' chars */
+void buf_cur_mvb(hBuffer buf, unsigned int n);
+/** Move cursor to EOL/EOB */
+unsigned int buf_cur_mveol(hBuffer buf);
+
+
+/** Return buffer properties */
+struct buf_props buf_get_props(hBuffer buf);
 /** Return the number of newlines '\n' in the buffer */
 unsigned int buf_get_linecount(hBuffer buf);
 /** Return size of the buffer data */
 unsigned int buf_get_size(hBuffer buf);
 /** Return char at pos. check buf_in_gap().*/
 char buf_get_char(hBuffer buf, unsigned int pos);
-/** Return buffer properties */
-struct buf_props buf_get_props(hBuffer buf);
 /** Save buffer to disk at path*/
 int buf_save_to_disk(hBuffer buf, const char* path);
-/** Update buffer. Update cache etc. */
-int buf_update(hBuffer buf);
 
 
 void buf_pprint(hBuffer buf);
