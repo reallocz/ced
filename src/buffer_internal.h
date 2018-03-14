b71038d8cfa46141f9b1d83021ee1ace1e743580
realloc <realloc@protonmail.com>
2018-03-14 23:47:13 +0530

    ":(  development gone wrong [Builds; doesnt work]"
    A lack of thorough planning lead to this mess where the buffer
    had to be completely rewritten.
    - This commit is NOT complete/correct. It just builds... fml

--- src/buffer_internal.h
+++ src/buffer_internal.h
@@ -3,23 +3,14 @@
 
 /** Static method headers */
 
-
 /** return a new id for a buffer */
-static
-unsigned int generate_id();
+static unsigned int generate_id();
 
 /** Increase buffer gap */
-static
-void gap_add(struct buffer* b);
+static void gap_add(struct buffer* buf);
 
-static
-int update_cache(struct buffer* b);
+/** Resize buffer gap if needed */
+static int gap_resize_if_required(struct buffer* buf);
 
-/** buf_cur_mv* functions just update buffer.cur. they don't move
- * the gap in memory (because that would be expensive)
- * The gap is moved only when addch / delch are called i.e. the buffer
- * is actually modified.
- * This function does exactly that - move's gap to cursor position
- */
-static
-int gap_sync(struct buffer* b);
+/** move gap to cursor */
+static void gap_move(struct buffer* buf, struct cursor cur);
--- src/fileutils.c
+++ src/fileutils.c
@@ -4,12 +4,62 @@
 #include <errno.h>
 #include <assert.h>
 #include <unistd.h>
+#include "log.h"
 
-char* fu_read_file(const char* path)
+#define TAG "FUTILS"
+
+struct file_stats fu_stats(const char* path)
 {
-    // TODO
-    return NULL;
+    struct file_stats fs;
+    fs.exists = 0;
+    fs.path = path;
+    fs.size = 0;
+    fs.type = F_FILE;
+
+    struct stat s;
+    int err = 0;
+    char abspath[PATH_MAX];
+    realpath(path, abspath);
+    err = stat(abspath, &s);
+    if(err) {
+        if(errno == ENOENT) {
+            log_l(TAG, "File: %s doesn't exist", path);
+        } else {
+            log_e(TAG, "Unknown error reading stat()");
+        }
+        return fs;
+    } else {
+        // man 7 inode
+        if(S_ISREG(s.st_mode)) { // S_IFREG = regular file
+            fs.type = F_FILE;
+        } else if(S_ISDIR(s.st_mode)) {
+            fs.type = F_DIR;
+        }
+        fs.path = path;
+        fs.abspath = abspath;
+        fs.size = s.st_size;
+        fs.exists = 1;
+        log_l(TAG, "%s: path: %s, apath: %s, fsize: %d, type: %s",
+                __func__, fs.path, fs.abspath, fs.size,
+                fs.type == F_FILE ? "file" : "dir");
+        return fs;
+    }
 }
+
+
+int fu_read_to_buffer(const char* path, char* buffer)
+{
+    FILE* f = fopen(path, "r");
+    assert(f);
+    long bytes = 0;
+    fseek(f, 0, SEEK_END);
+    bytes = ftell(f);
+    rewind(f);
+    fread(buffer, sizeof(char), bytes, f);
+    fclose(f);
+    return 0;
+}
+
 
 int fu_write_file(const char* path, const char* data)
 {
@@ -26,13 +76,13 @@ int fu_file_exists(const char* path)
 {
     struct stat s;
     int err = 0;
-    err = stat(path, &s);
-
     char abspath[PATH_MAX];
     realpath(path, abspath);
+    err = stat(abspath, &s);
+
     if(err) {
-        // Either it doesn't exist or we lack the permissions. Either way
-        // we failed
+        /* Either it doesn't exist or we lack the permissions.
+         * Either way we failed */
         return 0;
     }
     // man 7 inode
